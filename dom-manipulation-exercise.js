85e7d08fb67178eaf18637186d3d4f85c51201d3
Richard Webb <76513305+Rwebb1981@users.noreply.github.com>
2025-01-02 07:12:09 +0000

    Add HTML and JS code to enable buttons for events
    HTML and JS code were added to index and JS files to create buttons
    which provide alerts. Three methods were used, including EventListeners.

--- dom-manipulation-exercise.js
+++ dom-manipulation-exercise.js
@@ -46,3 +46,36 @@ newDiv.appendChild(paragraphInDiv);
 
 // 6. Append the new <div> to the container
 container.appendChild(newDiv);
+
+// the JavaScript file
+// METHODS 2 & 3
+function alertFunction() {
+    alert("YAY! YOU DID IT!");
+  }
+  const btn = document.querySelector("#btn");
+
+// METHOD 3
+btn.addEventListener("click", alertFunction);
+
+btn.addEventListener("click", function (e) {
+    console.log(e);
+  });
+
+btn.addEventListener("click", function (e) {
+    console.log(e.target);
+  });
+
+btn.addEventListener("click", function (e) {
+    e.target.style.background = "blue";
+  });
+
+// buttons is a node list. It looks and acts much like an array.
+const buttons = document.querySelectorAll("button");
+
+// we use the .forEach method to iterate through each button
+buttons.forEach((button) => {
+  // and for each one we add a 'click' listener
+  button.addEventListener("click", () => {
+    alert(button.id);
+  });
+});
